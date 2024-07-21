53a919845d3360c2d9016f7e04a956224c732527
kuang <kuanglee97@gmail.com>
2024-07-21 16:53:05 +0700

    fix electron build

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-require('dotenv').config(); // Load environment variables from .env file
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '.env') }); // Load environment variables from .env file
 const { app, BrowserWindow, ipcMain } = require('electron');
 const axios = require('axios');
 const FormData = require('form-data');
 const fs = require('fs');
-const path = require('path');
 
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: path.join(__dirname,"build", '/Sunflower_from_Silesia2.jpg'),
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
@@ -37,6 +38,7 @@ ipcMain.on('upload-file', (event, filePath) => {
   console.log('Received file path:', filePath);
   const apiKey = process.env.API_KEY;
   const url = process.env.ENDPOINT_API;
+
   const form = new FormData();
   const fileStream = fs.createReadStream(filePath);
   form.append('file_upload', fileStream);
@@ -46,9 +48,11 @@ ipcMain.on('upload-file', (event, filePath) => {
     headers: form.getHeaders()
   })
       .then(response => {
+        event.sender.send('upload-response', { success: true, message: response.data.message, data: response?.data?.data });
         console.log('File uploaded successfully:', response.data);
       })
       .catch(error => {
         console.error('Error uploading file:', error);
+        event.sender.send('upload-response', { success: false, message: error.data.message });
       });
 });
--- renderer.js
+++ renderer.js
@@ -1,7 +1,39 @@
 const { ipcRenderer } = require('electron');
 
+document.getElementById('fileInput').addEventListener('change', () => {
+    const fileInput = document.getElementById('fileInput');
+    const fileNameDiv = document.getElementById('fileName');
+
+    if (fileInput.files.length > 0) {
+        const file = fileInput.files[0];
+        fileNameDiv.textContent = `Selected file: ${file.name}`;
+    } else {
+        fileNameDiv.textContent = '';
+    }
+});
+
+// Example function to trigger download
+function downloadJsonFile(jsonData, fileName) {
+    // Create a Blob from the JSON data
+    if (jsonData === undefined){
+        console.log("json data ", jsonData)
+
+        return;
+    }
+    const blob = new Blob([JSON.stringify(jsonData, null, 2)], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+
+    // Create a link element
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = fileName;
+    a.click();
+
+    // Cleanup
+    URL.revokeObjectURL(url);
+}
+
 document.getElementById('uploadBtn').addEventListener('click', () => {
-    console.log(3333)
     const fileInput = document.getElementById('fileInput');
     if (fileInput.files.length > 0) {
         const file = fileInput.files[0];
@@ -12,4 +44,20 @@ document.getElementById('uploadBtn').addEventListener('click', () => {
     } else {
         alert('Please select a file to upload.');
     }
+});
+
+// Listen for upload response from main process
+ipcRenderer.on('upload-response', (event, response) => {
+    const fileInput = document.getElementById('fileInput');
+    const fileNameDiv = document.getElementById('fileName');
+    console.log(response)
+    if (response.success) {
+        alert(response.message);
+        downloadJsonFile(response.data, 'data.json');
+    } else {
+        alert(response.message);
+    }
+    // Clear the file input after submission
+    fileInput.value = '';
+    fileNameDiv.textContent = ''; // Clear the file name display
 });
